87df3c6912f3ec35be35ffb4a3782af3d117bea5
Michael Showes <michael.showes@gmail.com>
2023-03-02 02:21:04 -0500

    removed fetch request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import scss from './homepage.module.scss';
 
-import homepage from '@utils/data/homepage';
 import HomeHero from '@components/HomeHero/HomeHero';
 import GalleryTeaser from '@components/GalleryTeaser/GalleryTeaser';
 import BlogTeaser from '@components/BlogTeaser/BlogTeaser';
@@ -10,8 +9,6 @@ import Testimonial from '@components/Testimonial/Testimonial';
 type Props = {};
 
 export default async function Home({}: Props) {
-  const data = await homepage();
-
   return (
     <main className={scss.homepage}>
       <HomeHero />
