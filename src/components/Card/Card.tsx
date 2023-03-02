068db7c7a2d11fabbfd3b1927bfe52dae24e84d9
Michael Showes <michael.showes@gmail.com>
2023-03-02 01:37:46 -0500

    fixed type error

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,13 +8,23 @@ type Props = {
   title: string;
   text: string;
   url: string;
+  width: number;
+  height: number;
 };
 
-export default function Card({ image, altText, title, text, url }: Props) {
+export default function Card({
+  image,
+  altText,
+  title,
+  text,
+  url,
+  width,
+  height
+}: Props) {
   return (
     <article className={scss.card}>
       <div className={scss.image}>
-        <Image src={image} alt={altText} fill />
+        <Image src={image} alt={altText} fill width={width} height={height} />
       </div>
       <h3>{title}</h3>
       <p className={scss.teaserText}>{text}</p>
